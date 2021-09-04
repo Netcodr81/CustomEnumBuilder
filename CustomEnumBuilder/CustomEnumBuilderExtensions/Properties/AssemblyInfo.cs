83a1867e68ba62bb6459abfb4ab98c89e76e5897
Mike Eckel <mikeeckel@hotmail.com>
2021-09-04 13:42:47 -0500

    Nuget package files added for CustomEnumBuilderExtensions and CustomEnumBuilderCoreExtensions

--- CustomEnumBuilder/CustomEnumBuilderExtensions/Properties/AssemblyInfo.cs
+++ CustomEnumBuilder/CustomEnumBuilderExtensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomEnumBuilderExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CustomEnumBuilder.Extensions")]
+[assembly: AssemblyDescription("This package adds extensions for the CustomEnumBuilder.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomEnumBuilderExtensions")]
+[assembly: AssemblyCompany("NetCodr")]
+[assembly: AssemblyProduct("CustomEnumBuilder.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
